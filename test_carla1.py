b11ae08134645b5b41d9d06f1b628e945ed24f1b
BavoLesy <bavo.lesy@student.uantwerpen.be>
2022-11-24 15:35:01 +0100

    testing carla

--- test_carla1.py
+++ test_carla1.py
@@ -6,7 +6,10 @@ def carla_API():
     #world = client.get_world()
     # Change state to synchronous mode when you want to control the simulation (when deploying the agent)
     # Load town 01 map
-    world = client.load_world('Town01')
+    world = client.load_world('Town02')
+    blueprint_library = world.get_blueprint_library()
+    vehicle_bp = blueprint_library.filter('model3')[0]
+    vehicle = world.spawn_actor(vehicle_bp, carla.Transform(carla.Location(x=0, y=0, z=2), carla.Rotation(yaw=0)))
 
 
 if __name__ == '__main__':
