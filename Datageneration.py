72710cea68f5104f33675d159d8f046b572e2e15
BavoLesy <bavo.lesy@student.uantwerpen.be>
2022-12-04 22:10:50 +0100

    testing carla

--- Datageneration.py
+++ Datageneration.py
@@ -76,8 +76,8 @@ def main(town):
     # Simulator
     client = carla.Client('localhost', 2000)
     client.set_timeout(10.0)
-    world = client.load_world(town)
-    #world = client.get_world()
+    #world = client.load_world(town)
+    world = client.get_world()
     blueprint_library = world.get_blueprint_library()
     spawn_points = world.get_map().get_spawn_points()
 
